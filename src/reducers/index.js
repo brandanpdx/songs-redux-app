431d025167a73949e0e0e5a76d5b0b51ed55a397
Brandan Sayarath <brandan@brandan.tech>
2020-12-05 17:05:58 -0800

    add songsReducer with an arry of objects of songs

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+const songsReducer = () => {
+  return [
+    {
+      title: "Sun & Moon",
+      duration: "4:05",
+    },
+    {
+      title: "Peace of Mind",
+      duration: "3:25",
+    },
+    {
+      title: "We're All We Need",
+      duration: "6:22",
+    },
+    {
+      title: "Blue Sky Action",
+      duration: "5:53",
+    },
+  ];
+};
