dd818567c98abf998bebd23674144f4f7783d6ee
Brandan Sayarath <brandan@brandan.tech>
2020-12-05 17:24:04 -0800

    add selectedSongReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,3 +18,10 @@ const songsReducer = () => {
     },
   ];
 };
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
